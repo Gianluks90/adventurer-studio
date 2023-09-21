50ac5d897601d704501d902ab65fc7457120f95f
JMIKEPAD <mchparodi@gmail.com>
2023-09-21 12:10:41 +0200

    home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { getAuth } from 'firebase/auth';
+import { FormModel } from 'src/app/models/formModel';
+import { CharacterService } from 'src/app/services/character.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
@@ -8,11 +11,19 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  public form : FormGroup = this.fb.group(FormModel.create(this.fb))
 
-  constructor(public firebaseService: FirebaseService) {}
+  constructor(public firebaseService: FirebaseService, private charService: CharacterService, private fb:FormBuilder) {}
 
   public logout() {
     getAuth().signOut();
     // window.location.reload();
+  }
+
+  public createCharacter(){
+    this.charService.createCharacter(this.form).then(()=> {
+      console.log('fatto');
+
+    })
   }
 }
