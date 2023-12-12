432947c4676fa574e6124e2fb75579b85568f667
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2023-12-12 11:31:54 +0100

    Aggiornata settings tab in char view

--- src/app/components/character-view/sub-components/settings-tab-view/settings-tab-view.component.ts
+++ src/app/components/character-view/sub-components/settings-tab-view/settings-tab-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CharacterService } from 'src/app/services/character.service';
+import { FormService } from 'src/app/services/form.service';
 
 @Component({
   selector: 'app-settings-tab-view',
@@ -10,11 +11,9 @@ export class SettingsTabViewComponent {
 
   sheetColorData: string = '';
 
-  constructor(private charService: CharacterService) { }
+  constructor(private charService: CharacterService, private formService: FormService) { }
   
   @Input() set sheetColor(sheetColor: string) {
-    console.log('sheetColor', sheetColor);
-    
     this.sheetColorData = sheetColor.split('40')[0];
   }
 
@@ -29,5 +28,15 @@ export class SettingsTabViewComponent {
     this.charService.updateCharacterSheetColorById(window.location.href.split('/').pop()!, '#FFFFFF40').then(() => {
       window.location.reload();
     });
+  }
+
+  public parseExport() {
+    const jsonFile = JSON.stringify(this.formService.formSubject.value.value);
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(jsonFile);
+    const exportButton = document.getElementById('export-button');
+    const characterName = this.formService.formSubject.value.value.informazioniBase.nomePersonaggio.split(' ')[0];
+    const fileName = characterName + '_' + (new Date().getTime().toString()) + '.json';
+    exportButton.setAttribute("href", dataStr );
+    exportButton.setAttribute("download", fileName);
   }
 }
