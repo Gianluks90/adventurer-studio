df4975d2b0ad366f8209afb5f8a4a728d509583d
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2023-12-13 14:12:58 +0100

    fixed scroll inventario
    + Cinzia è stata qui

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -4,24 +4,26 @@ export class Item {
     filtered: boolean;
     quantity: number;
     description?: string;
+    cost?: number;
 
-    constructor(name: string, icon: string, filtered: boolean, quantity: number, description?: string) {
+    constructor(name: string, icon: string, filtered: boolean, quantity: number, description?: string, cost?: number) {
         this.name = name;
         this.icon = icon;
         this.filtered = filtered;
         this.quantity = quantity;
         this.description = description;
+        this.cost = cost;
     }
 
     static createEmpty() {
         return new Item('', '', false, 0, '');
     }
 
-    static create(name: string, icon: string, filtered: boolean, quantity: number, description?: string) {
-        return new Item(name, icon, filtered, quantity, description);
+    static create(name: string, icon: string, filtered: boolean, quantity: number, description?: string, cost?: number) {
+        return new Item(name, icon, filtered, quantity, description, cost);
     }
 
     static createFrom(item: Item) {
-        return new Item(item.name, item.icon, item.filtered, item.quantity, item.description);
+        return new Item(item.name, item.icon, item.filtered, item.quantity, item.description, item.cost);
     }
 }
