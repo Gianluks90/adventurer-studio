7f6aea38ef28d5512d6ab1d8f8c9eb7e37aeb5fe
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2023-12-15 12:03:06 +0100

    Update item model

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -3,27 +3,68 @@ export class Item {
     icon: string;
     filtered: boolean;
     quantity: number;
+    rarity: string;
+    category: string;
     description?: string;
-    cost?: number;
+    value?: string;
+    weight?: number;
+    cursed?: boolean;
+    cursedDescription?: string;
+    traits: Trait[];
+    attunementRequired?: boolean;
+    attuned?: boolean;
+    artifactProperties?: string[];
+    reference?: string;
 
-    constructor(name: string, icon: string, filtered: boolean, quantity: number, description?: string, cost?: number) {
+    constructor(name: string, icon: string, filtered: boolean, quantity: number, rarity: string, category: string, description?: string, value?: string, weight?: number, cursed?: boolean, cursedDescription?: string, traits?: Trait[], attunementRequired?: boolean, attuned?: boolean, artifactProperties?: string[], reference?: string) {
         this.name = name;
         this.icon = icon;
         this.filtered = filtered;
         this.quantity = quantity;
+        this.rarity = rarity;
+        this.category = category;
         this.description = description;
-        this.cost = cost;
+        this.value = value;
+        this.weight = weight;
+        this.cursed = cursed;
+        this.cursedDescription = cursedDescription;
+        this.traits = traits;
+        this.attunementRequired = attunementRequired;
+        this.attuned = attuned;
+        this.artifactProperties = artifactProperties;
+        this.reference = reference;
     }
 
-    static createEmpty() {
-        return new Item('', '', false, 0, '');
+    static parseFromData(data: any): Item {
+        return new Item(data.name, data.icon, data.filtered, data.quantity, data.rarity, data.category, data.description, data.value, data.weight, data.cursed, data.cursedDescription, data.traits, data.attunementRequired, data.attuned, data.artifactProperties, data.reference);
     }
+}
 
-    static create(name: string, icon: string, filtered: boolean, quantity: number, description?: string, cost?: number) {
-        return new Item(name, icon, filtered, quantity, description, cost);
+export class Trait {
+    title: string;
+    description: string;
+}
+
+export class Armor extends Item {
+    CA: number;
+    stelthDisadvantage: boolean;
+
+    constructor(name: string, icon: string, filtered: boolean, quantity: number, rarity: string, category: string, description?: string, value?: string, weight?: number, cursed?: boolean, cursedDescription?: string, traits?: Trait[], attunementRequired?: boolean, attuned?: boolean, artifactProperties?: string[], reference?: string, CA?: number, stelthDisadvantage?: boolean) {
+        super(name, icon, filtered, quantity, rarity, category, description, value, weight, cursed, cursedDescription, traits, attunementRequired, attuned, artifactProperties, reference);
+        this.CA = CA;
+        this.stelthDisadvantage = stelthDisadvantage;
     }
+}
 
-    static createFrom(item: Item) {
-        return new Item(item.name, item.icon, item.filtered, item.quantity, item.description, item.cost);
+export class Weapon extends Item {
+    damageFormula: string;
+    damageType: string;
+    weaponProperties: string[];
+
+    constructor(name: string, icon: string, filtered: boolean, quantity: number, rarity: string, category: string, description?: string, value?: string, weight?: number, cursed?: boolean, cursedDescription?: string, traits?: Trait[], attunementRequired?: boolean, attuned?: boolean, artifactProperties?: string[], reference?: string, damageFormula?: string, damageType?: string, weaponProperties?: string[]) {
+        super(name, icon, filtered, quantity, rarity, category, description, value, weight, cursed, cursedDescription, traits, attunementRequired, attuned, artifactProperties, reference);
+        this.damageFormula = damageFormula;
+        this.damageType = damageType;
+        this.weaponProperties = weaponProperties;
     }
 }
